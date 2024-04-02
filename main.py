878aedd529513c80fbca494967ae4418ca5130a4
swbaek97 <80388349+swbaek97@users.noreply.github.com>
2024-04-02 13:30:55 +0000

    baekjoon solve level1

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import time
+#import time
 
-start_time = time.time()  # 측정 시작
+#start_time = time.time()  # 측정 시작
 ################################################################
 # 내가 푼 풀이
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )\"\"\"\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
 
 # 답지 풀이
 
 ################################################################
-end_time = time.time()  # 측정 종료
-print("time:", end_time - start_time)  # 수행 시간 출력
+#end_time = time.time()  # 측정 종료
+#print("time:", end_time - start_time)  # 수행 시간 출력
